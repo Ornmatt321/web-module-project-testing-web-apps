0d506bb7891d8a9b66c35bf9d3e6a1f26c13bea2
Matt Orndorff <ornmatt321@gmail.com>
2022-04-14 14:43:27 -0400

    initialSubmit

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -5,7 +5,7 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', () => {
-
+//initial submit
 });
 
 test('renders the contact form header', () => {
